095bd95935f111c743c2415846d80b0e1e09342d
Sam BK <32336398+sbevanskerr@users.noreply.github.com>
2019-09-18 18:40:54 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    db: {
-        uri: 'mongodb+srv://guest:pass1@cluster0-nv7ux.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    }
-};
